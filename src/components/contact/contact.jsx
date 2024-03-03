712bad14d72ea0cd376e16f1836f5c5569d97ef1
Jake Gehrke <gehrkej@oregonstate.edu>
2024-03-02 20:48:59 -0800

    added email

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
 
 const contact = () => {
-    return (
-        <section id='contact'>Contact</section>
-    )
-}
+  return (
+    <section id="contact">
+      <h5>Want to Get in touch?</h5>
+      <h2>Contact Me:</h2>
 
-export default contact
+      <div className="container contact__container">
+        <div className="experience__frontend">
+          <h3>My Contact Information</h3>
+          <div className="contact__content">
+            <article className="contact__details">
+              <div>
+                <h4>Email:</h4>
+                <small className="text-light">jake.d.gehrke@gmail.com</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default contact;
