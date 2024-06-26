8978118250f18b879ad39323c9b1a56106e2d6a7
Jake Gehrke <gehrkej@oregonstate.edu>
2024-06-26 09:43:14 -0700

    updated experience content

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './experience.css'
-import { BsPatchCheckFill } from 'react-icons/bs'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const experience = () => {
     return (
@@ -16,7 +16,7 @@ const experience = () => {
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
                                 <h4>HTML</h4>
-                                <small className='text-light'>Experienced</small>
+                                <small className='text-light'>3 Years</small>
                             </div>
 
                         </article>
@@ -24,14 +24,14 @@ const experience = () => {
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
                                 <h4>CSS</h4>
-                                <small className='text-light'>Intermediate</small>
+                                <small className='text-light'>3 Years</small>
                             </div>
                         </article>
                         <article className='experience__details'>
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
                                 <h4>JavaScript</h4>
-                                <small className='text-light'>Intermediate</small>
+                                <small className='text-light'>3 Years</small>
                             </div>
 
                         </article>
@@ -39,15 +39,32 @@ const experience = () => {
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
                                 <h4>React</h4>
-                                <small className='text-light'>Intermediate</small>
+                                <small className='text-light'>2 Years</small>
                             </div>
 
                         </article>
+
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>React Native</h4>
+                                <small className='text-light'>1 Years</small>
+                            </div>
+                        </article>
+
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Figma</h4>
+                                <small className='text-light'>1 Years</small>
+                            </div>
+                        </article>
+
                         <article className='experience__details'>
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
                                 <h4>Kotlin</h4>
-                                <small className='text-light'>Intermediate</small>
+                                <small className='text-light'>~1 Years</small>
                             </div>
 
                         </article>
@@ -55,9 +72,10 @@ const experience = () => {
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
                                 <h4>Emotion</h4>
-                                <small className='text-light'>Intermediate</small>
+                                <small className='text-light'>~1 Years</small>
                             </div>
                         </article>
+
                     </div>
 
                 </div>
@@ -70,7 +88,7 @@ const experience = () => {
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
                                 <h4>Node JS</h4>
-                            <small className='text-light'>Experienced</small>
+                                <small className='text-light'>2 Years</small>
                             </div>
                         </article>
                         <article className='experience__details'>
@@ -83,20 +101,151 @@ const experience = () => {
                         <article className='experience__details'>
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
-                                <h4>C/C++</h4>
-                            <small className='text-light'>Experienced</small>
-                            </div>                   
+                                <h4>SQL</h4>
+                                <small className='text-light'>1 Year</small>
+                            </div>
                         </article>
                         <article className='experience__details'>
                             <BsPatchCheckFill className='experience__details-icons'/>
                             <div>
-                                <h4>Python</h4>
-                                <small className='text-light'>Intermediate</small>
+                                <h4>Google Firebase</h4>
+                                <small className='text-light'>1 Year</small>
                             </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>MongoDB</h4>
+                                <small className='text-light'>~1 Year</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>RabbitMQ</h4>
+                                <small className='text-light'>~1 Year</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Redis</h4>
+                                <small className='text-light'>~1 Year</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Docker</h4>
+                                <small className='text-light'>~1 Year</small>
+                            </div>
+
                         </article>
                     </div>
                 </div>
+                {/*End Backend*/}
+
+                <div className="experience__Programming">
+                    <h3>Other Programming</h3>
+                    <div className="experience__content">
+
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>C/C++</h4>
+                                <small className='text-light'>3 Years</small>
+                            </div>
+                        </article>
+
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Python</h4>
+                                <small className='text-light'>1 Year</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>C#</h4>
+                                <small className='text-light'>~1 Year</small>
+                            </div>
+
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Assembly</h4>
+                                <small className='text-light'>~1 Year</small>
+                            </div>
+
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Unity</h4>
+                                <small className='text-light'>~1 Year</small>
+                            </div>
+                        </article>
+                    </div>
+
+                </div>
+                {/*End Programming*/}
+
+                <div className="experience__Programming">
+                    <h3>Business Experience</h3>
+                    <div className="experience__content">
+
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Management</h4>
+                                <small className='text-light'>3 Years</small>
+                            </div>
+                        </article>
+
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Project Management</h4>
+                                <small className='text-light'>2 Year</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Marketing</h4>
+                                <small className='text-light'>2 Year</small>
+                            </div>
+
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Social Media</h4>
+                                <small className='text-light'>2 Year</small>
+                            </div>
+
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Budgeting</h4>
+                                <small className='text-light'>1 Year</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icons'/>
+                            <div>
+                                <h4>Accounting</h4>
+                                <small className='text-light'>1 Year</small>
+                            </div>
+                        </article>
+                    </div>
+
+                </div>
             </div>
+
 
         </section>
     )
