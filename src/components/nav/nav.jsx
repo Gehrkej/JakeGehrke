49a34e22daa41fdd139395547cb8cf6cf330018f
Jake Gehrke <gehrkej@oregonstate.edu>
2024-06-27 13:12:58 -0700

    added portfolio to nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BiBook} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
-import {BiMessageSquareDetail} from 'react-icons/bi'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { RiServiceLine } from 'react-icons/ri'
+import { BiMessageSquareDetail } from 'react-icons/bi'
+import { IoFileTrayStackedOutline } from 'react-icons/io5'
+
 // import { useState } from 'react'
 
 const nav = () => {
     // const [activeNav, setActiveNav] = useState('#') 
     return (
         <nav>
-            <a href="#"><AiOutlineHome/></a>
-            <a href="#about"><AiOutlineUser/></a>
-            <a href="#experience"><BiBook/></a>
-            <a href="#services" ><RiServiceLine/></a>
-            <a href="#contact"><BiMessageSquareDetail/></a>
+            <a href="#"><AiOutlineHome /></a>
+            <a href="#about"><AiOutlineUser /></a>
+            <a href="#experience"><BiBook /></a>
+            <a href="#services"><RiServiceLine /></a>
+            <a href="#portfolio"><IoFileTrayStackedOutline /></a>
+            <a href="#contact"><BiMessageSquareDetail /></a>
         </nav>
     )
 }
