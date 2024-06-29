1f9b8e62a37451566ef1728df34946ff7c0fd31f
Jake Gehrke <gehrkej@oregonstate.edu>
2024-06-28 18:50:27 -0700

    removed basename to browser router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/build">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
