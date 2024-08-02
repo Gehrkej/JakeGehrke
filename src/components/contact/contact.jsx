8c67b2ea951f4ba7afac366a15ada0977c724643
Jake Gehrke <gehrkej@oregonstate.edu>
2024-08-02 11:54:42 -0700

    add - added animation for contact section

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef } from 'react'
 import "./contact.css";
-const contact = () => {
-  return (
+import {motion, useAnimation } from 'framer-motion'
+const Contact = () => {
+    const controls = useAnimation();
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    controls.start({
+                        y: 0,
+                        opacity: 1,
+                        transition: { duration: 1 }
+                    });
+                }
+            },
+            {
+                threshold: [0.1, 0.25, 0.5, 0.75, 1.0]
+            }
+        );
+
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+
+        // Cleanup the observer on component unmount
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, [controls]);
+
+
+    return (
     <section id="contact">
       <h5>Want to Get in touch?</h5>
       <h2>Contact Me:</h2>
 
-      <div className="container contact__container">
+      <motion.div className="container contact__container"
+                  ref={ref}
+                  initial={{ y: 100, opacity: 0 }}
+                  animate={controls}>
         <div className="experience__frontend">
           <h3>My Contact Information</h3>
           <div className="contact__content">
@@ -18,9 +54,9 @@ const contact = () => {
             </article>
           </div>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
 
-export default contact;
+export default Contact;
