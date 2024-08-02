fb7ab28e19fbdaa669db907ec3960586d50a29d0
Jake Gehrke <gehrkej@oregonstate.edu>
2024-08-02 10:16:00 -0700

    add - added tooltip to nav icons

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -6,20 +6,48 @@ import { BiBook } from 'react-icons/bi'
 import { RiServiceLine } from 'react-icons/ri'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 import { IoFileTrayStackedOutline } from 'react-icons/io5'
+import { Tooltip as ReactTooltip } from "react-tooltip";
+
 
 // import { useState } from 'react'
 
 const nav = () => {
     // const [activeNav, setActiveNav] = useState('#') 
     return (
-        <nav>
-            <a href="#home"><AiOutlineHome /></a>
-            <a href="#about"><AiOutlineUser /></a>
-            <a href="#experience"><BiBook /></a>
-            <a href="#portfolio"><IoFileTrayStackedOutline /></a>
-            <a href="#services"><RiServiceLine /></a>
-            <a href="#contact"><BiMessageSquareDetail /></a>
+         <>
+             <nav>
+            <a href="#home" data-tooltip-id='home_tooltip'><AiOutlineHome /></a>
+            <a href="#about" data-tooltip-id='about_tooltip'><AiOutlineUser /></a>
+            <a href="#experience" data-tooltip-id='experience_tooltip'><BiBook /></a>
+            <a href="#portfolio" data-tooltip-id='portfolio_tooltip'><IoFileTrayStackedOutline /></a>
+            <a href="#services" data-tooltip-id='services_tooltip'><RiServiceLine /></a>
+            <a href="#contact" data-tooltip-id='contact_tooltip'><BiMessageSquareDetail /></a>
         </nav>
+            <ReactTooltip
+                id='home_tooltip'
+                place="top"
+                content="Home" />
+             <ReactTooltip
+                 id='about_tooltip'
+                 place="top"
+                 content="About" />
+             <ReactTooltip
+                 id='experience_tooltip'
+                 place="top"
+                 content="Experience" />
+             <ReactTooltip
+                 id='portfolio_tooltip'
+                 place="top"
+                 content="Portfolio" />
+             <ReactTooltip
+                 id='services_tooltip'
+                 place="top"
+                 content="Services" />
+             <ReactTooltip
+                 id='contact_tooltip'
+                 place="top"
+                 content="Contact" />
+             </>
     )
 }
 
